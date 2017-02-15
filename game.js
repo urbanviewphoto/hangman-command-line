91d7cf4a4232f3520e263e94f64f34a5064a7d43
Frank Villafane <urbanviewphoto@gmail.com>
2017-02-15 18:23:49 -0500

    update game.js

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 // Hangman CLI
 //    Consists of 
-//		Word 		hidden word the user must guess (computer assigend)
+//		gWord 		hidden word the user must guess (computer assigend)
 //    	Letter		a letter entered. 
 //					if correct, show letter in proper position in word
 //                  if not, do not change
-//		Words		array of words that are used by the game 
-//					and assigned randomly (for now will be 10 words)
+//		Words		array of word objects that are used by the game 
+//					and assigned randomly (for now will be 10 objects)
 //		Topic		words will be about this topic, also randomly assigned
 //					this will act as the hint
-//		bWord		blank representation of Word (_ _ _ _ _ _ )
+//		gDisp		blank representation of Word (_ _ _ _ _ _ )
 //		guesses		number of guesses allowed to guess the Word 
 //					one guess, if correct equals the letter(s) found
 //					if incorrect, reduces number of guesses
@@ -72,18 +72,18 @@ var Words = [{ gTopic: Topics[0],
 			   gHint: 'Term depicting a 35mm format camera'
 			 }];
 
-var idx = Math.floor(Math.random() * Words.length);
-var wordsObj = Words[idx]; 
-
-var guessWord = wordsObj.gWord;
-var guessDisp = wordsObj.gDisp;
-var guesses   = guessWord.length;
-
-var wordObject = new Word(guessWord);
-wordObject.makeAndPushLettersIntoWord();
-
 // Begin the display
 function displayScreen(){
+	var idx = Math.floor(Math.random() * Words.length);
+	var wordsObj = Words[idx]; 
+
+	guessWord = wordsObj.gWord;
+	var guessDisp = wordsObj.gDisp;
+	var guesses   = guessWord.length;
+
+	var wordObject = new Word(guessWord);
+	wordObject.makeAndPushLettersIntoWord();
+
 	process.stdout.write('\033c'); // This clears the screen each time 
 	console.log('\n\nYour topic is --> ' + wordsObj.gTopic + '\n'); 
 	console.log('Your hint is ---> ' + wordsObj.gHint + '\n');
@@ -114,6 +114,21 @@ function askLetter(){
     			console.log("That's Right!!"); 
     		else 
     			console.log("No, that's not it!");
+    	}
+    	playAgain(); 
+    });
+}
+
+function playAgain(){
+    inquirer.prompt([
+    {
+    type: "input",
+    name: "replay",
+    message: "Want to play again? (y or n)"},
+    ]).then(function(data){
+    	if (data.replay == 'y'){
+    		displayScreen();
+    		askLetter();      		
     	}
     });
 }
